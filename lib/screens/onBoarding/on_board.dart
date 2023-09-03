63b9669168b5386d422239e0ce246a6f5613c982
Suhaibusman <suhaibusman54@gmail.com>
2023-09-03 07:54:46 -0700

    mini app

--- lib/screens/onBoarding/on_board.dart
+++ lib/screens/onBoarding/on_board.dart
@@ -19,51 +19,54 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
     return Scaffold(
            backgroundColor: const Color(0xff2A4BA0),
            body: SafeArea(
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height*0.5,
-              child: PageView.builder(
-                controller: _pageController,
-                itemCount: topBar.length,
-                itemBuilder: (context, index) {
-                return Container(
-                  
-                color: Colors.transparent,
-                           height: MediaQuery.of(context).size.height*0.4,
-                           width: MediaQuery.of(context).size.width,
-                child:  Padding(
-                  padding: const EdgeInsets.only(left: 42 , top: 20 ,right:50),
-                  child: Column(
-                     crossAxisAlignment: CrossAxisAlignment.start,
-                     mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      // OnBoardText(textData: "Your holiday shopping delivered to Screen ${topBar[index]}"),
-                      const OnBoardText(textData: "Your holiday"),
-                      const OnBoardText(textData: "shopping"),
-                      const OnBoardText(textData: "delivered to Screen"),
-                      Row(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height*0.5,
+                  child: PageView.builder(
+                    controller: _pageController,
+                    itemCount: topBar.length,
+                    itemBuilder: (context, index) {
+                    return Container(
+                      
+                    color: Colors.black,
+                               height: MediaQuery.of(context).size.height*0.4,
+                               width: double.infinity,
+                    child:  Padding(
+                      padding: const EdgeInsets.only(left: 42 , top: 20 ,right:50),
+                      child: Column(
+                         crossAxisAlignment: CrossAxisAlignment.start,
+                         mainAxisSize: MainAxisSize.min,
+
                         children: [
-                          OnBoardText(textData: " ${topBar[index]}"),
+                           OnBoardText(textData: "Your holiday shopping delivered to Screen ${topBar[index]}"),
+      
                           Padding(
                             padding: const EdgeInsets.only(left: 20),
                             child: SvgPicture.asset("assets/images/Emoji.svg"),
-                          ) ,
+                          ),
+                          
+                           Padding(
+                        padding: const EdgeInsets.only(top: 8),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "There's something for everyone to enjoy with Sweet Shop Favourites Screen ${str2[index]}",
+                            style: const TextStyle(color: Color(0xffB2BBCE), fontSize: 18),
+                          ),
+                        ),
+                      ),
+                      const Divider(
+                        color: Colors.blue,
+                        thickness :3,
+                        
+                      )
                         ],
                       ),
-                      
-                       Padding(
-                    padding: const EdgeInsets.only(top: 8),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "There's something for everyone to enjoy with Sweet Shop Favourites Screen ${str2[index]}",
-                        style: const TextStyle(color: Color(0xffB2BBCE), fontSize: 18),
-                      ),
                     ),
-                  ),
-                    ],
-                  ),
+                    );             },)
                 ),
-                );             },)
+              ],
             ),
            )
     );
