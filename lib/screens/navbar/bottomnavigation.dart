a35cd20eff1de4846af93a77d5d535458c6627c7
Suhaibusman <suhaibusman54@gmail.com>
2023-09-23 02:36:54 -0700

    mini app

--- lib/screens/navbar/bottomnavigation.dart
+++ lib/screens/navbar/bottomnavigation.dart
@@ -3,23 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:miniapp/screens/checkoutpage/cartscreen.dart';
 import 'package:miniapp/screens/favourite/favouritescreen.dart';
 import 'package:miniapp/screens/home/homescreen.dart';
-
-
 import '../category/category.dart';
 
 class CustomBottomNavigationBar extends StatefulWidget {
   final int pageindex;
-  const CustomBottomNavigationBar({super.key, required this.pageindex,  });
+
+  const CustomBottomNavigationBar({Key? key, required this.pageindex}) : super(key: key);
 
   @override
   State<CustomBottomNavigationBar> createState() => _CustomBottomNavigationBarState();
-
 }
+
 class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
   int currentindex = 0;
   GlobalKey<CurvedNavigationBarState> bottomNavigationKey = GlobalKey();
-  
-  final  List<Widget>  items = [
+
+  final List<Widget> items = [
     const Icon(Icons.add, size: 30),
     const Icon(Icons.category, size: 30),
     const Icon(Icons.favorite, size: 30),
@@ -33,49 +32,41 @@ class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
     const CartScreenData(),
   ];
 
-
+  @override
+  void initState() {
+    super.initState();
+    currentindex = widget.pageindex;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return 
-    CurvedNavigationBar(
+    return CurvedNavigationBar(
       key: bottomNavigationKey,
-        backgroundColor: const Color(0xffF8F7FB),
-        buttonBackgroundColor: Colors.amber,
-        animationCurve: Curves.easeInOut,
-        animationDuration: const Duration(milliseconds: 300),
-        items: items,
-        height: 70,
-          index: currentindex,
-          onTap: (selectedindex) {
-
-              setState(() {
-              currentindex = selectedindex;
-            });
-  if (selectedindex == 0) {
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomeScreen()));
-      setState(() {
-    currentindex = selectedindex;
-  });
-  } else if (selectedindex == 1) {
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const CategoryScreenData()));
-      setState(() {
-    currentindex = selectedindex;
-  });
-  } else if (selectedindex == 2) {
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const FavouriteScreenData()));
-      setState(() {
-    currentindex = selectedindex;
-  });
-  } else if (selectedindex == 3) {
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const CartScreenData()));
-      setState(() {
-    currentindex = selectedindex;
-  });
-  }
-},
-      );
-     
-    
+      backgroundColor: const Color(0xffF8F7FB),
+      buttonBackgroundColor: Colors.amber,
+      animationCurve: Curves.easeInOut,
+      animationDuration: const Duration(milliseconds: 300),
+      items: items,
+      height: 70,
+      index: currentindex, // Set the initial selected index
+      onTap: (pageindex) {
+        setState(() {
+          currentindex = pageindex;
+        });
+        if (pageindex ==0) {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeScreen(),));
+        } else  if (pageindex ==1) {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const CategoryScreenData(),));
+        } 
+        else  if (pageindex ==2) {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const FavouriteScreenData(),));
+        } 
+        else  if (pageindex ==3) {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const CartScreenData(),));
+        } 
+        // Update the selected screen based on the index
+        // You don't need to use Navigator.pushReplacement for this purpose
+      },
+    );
   }
 }
