601487bf30786d38fb632bc0934a598ada14f53c
Suhaibusman <suhaibusman54@gmail.com>
2023-09-24 09:08:55 -0700

    mini app

--- lib/screens/checkoutpage/addcarscreen.dart
+++ lib/screens/checkoutpage/addcarscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:miniapp/screens/checkoutpage/widgets/checkoutfunctions.dart';
 
 class AddCard extends StatefulWidget {
   const AddCard({super.key});
@@ -10,20 +11,32 @@ class AddCard extends StatefulWidget {
 class _AddCardState extends State<AddCard> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-             Row(
-              children: [
-                InkWell(
-                  onTap: () => Navigator.pop(context),
-                  child: Image.asset("assets/images/back.png")) ,
-                const Text("Add Card" ,style: TextStyle( fontFamily: "Manrope" ,fontSize: 16 ,fontWeight: FontWeight.w400),),
-               
-      
-              ],
-            ),
-        ],
+    return SafeArea(
+      child: Scaffold(
+        
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+               Row(
+                children: [
+                  InkWell(
+                    onTap: () => Navigator.pop(context),
+                    child: Image.asset("assets/images/back.png")) ,
+                  const Text("Add Card" ,style: TextStyle( fontFamily: "Manrope" ,fontSize: 16 ,fontWeight: FontWeight.w400),),
+    
+                ],
+    
+              ),
+              const SizedBox(height: 60,),
+              Container(
+                color: Colors.white,
+                height: 242,
+                width: 335,
+                child: Image.asset("assets/images/carddetails.png")),
+              const CheckoutFunction(),
+
+          ],
+        ),
       ),
     );
   }
